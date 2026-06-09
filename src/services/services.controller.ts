a89034ca28c76633c993a1a1fa4a2e9a4031eb16
Abi <alexis524@live.com.mx>
2026-06-08 18:11:39 -0600

    Agregar paginacion en servicios

--- src/services/services.controller.ts
+++ src/services/services.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   Patch,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { Role } from '@prisma/client';
@@ -13,6 +14,7 @@ import { Roles } from '../auth/decorators/roles.decorator';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { CreateServiceDto } from './dto/create-service.dto';
+import { GetServicesQueryDto } from './dto/get-services-query.dto';
 import { UpdateServiceDto } from './dto/update-service.dto';
 import { ServicesService } from './services.service';
 
@@ -30,22 +32,25 @@ export class ServicesController {
   }
 
   /**
+   * Ruta privada.
+   * Solo ADMIN puede ver todos los servicios.
+   *
+   * Soporta paginación:
+   * /services?page=1&limit=10
+   */
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @Get()
+  findAll(@Query() query: GetServicesQueryDto) {
+    return this.servicesService.findAll(query);
+  }
+
+  /**
    * Ruta pública para ver un servicio específico.
    */
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.servicesService.findOne(Number(id));
-  }
-
-  /**
-   * Ruta privada.
-   * Solo ADMIN puede ver todos los servicios.
-   */
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles(Role.ADMIN)
-  @Get()
-  findAll() {
-    return this.servicesService.findAll();
   }
 
   /**
